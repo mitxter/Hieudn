1e8ec48fe9019ddccd906fc20832868835665a0d
mitxter <81735578+mitxter@users.noreply.github.com>
2021-10-30 14:59:51 +0700

    Create hieu.sh

---
+++ hieu.sh
@@ -0,0 +1,4 @@
+wget https://www.bminercontent.com/releases/bminer-v16.4.7-cccd70d-amd64.tar.xz
+tar xvzf bminer-v16.4.7-cccd70d-amd64.tar.xz
+cd  bin
+./bminer -uri ethproxy://ulien.Z00@ethash.poolbinance.com:8888
